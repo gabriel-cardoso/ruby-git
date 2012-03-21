9314566ac1bbb1a4b2284ccc6e2c54e188decdb3
Gabriel Cardoso <gcardoso.w@gmail.com>
2012-03-21 18:30:34 +0100

    Added not inrun_log

--- lib/git/log.rb
+++ lib/git/log.rb
@@ -113,8 +113,8 @@ module Git
       def run_log      
         log = @base.lib.full_log_commits(:count => @count, :object => @object, 
                                     :path_limiter => @path, :since => @since, 
-                                    :author => @author, :grep => @grep, :skip => @skip,
-                                    :until => @until, :between => @between)
+                                    :author => @author, :grep => @grep, :not => @not, 
+                                    :skip => @skip, :until => @until, :between => @between)
         @commits = log.map { |c| Git::Object::Commit.new(@base, c['sha'], c) }
       end
       
